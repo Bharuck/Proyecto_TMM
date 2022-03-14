c6a05ddfb6cab10a6e89007483b6ff2ad288509a
William Parker <wlhh.2001@gmail.com>
2022-03-14 09:26:05 -0500

    Editando la pagina de calculos

--- gui/uis/pages/ui_main_pages.py
+++ gui/uis/pages/ui_main_pages.py
@@ -8,7 +8,16 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from qt_core import *
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QLineEdit, QSizePolicy, QStackedWidget, QVBoxLayout,
+    QWidget)
 
 class Ui_MainPages(object):
     def setupUi(self, MainPages):
@@ -31,11 +40,192 @@ class Ui_MainPages(object):
         self.pages.addWidget(self.page_1)
         self.page_2 = QWidget()
         self.page_2.setObjectName(u"page_2")
-        self.page_2.setStyleSheet(u"font-size: 14pt;background:orange;")
+        self.page_2.setStyleSheet(u"font-size: 14pt;")
         self.page_2_layout = QVBoxLayout(self.page_2)
         self.page_2_layout.setSpacing(5)
         self.page_2_layout.setObjectName(u"page_2_layout")
         self.page_2_layout.setContentsMargins(5, 5, 5, 5)
+        self.frame = QFrame(self.page_2)
+        self.frame.setObjectName(u"frame")
+        self.frame.setEnabled(False)
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.frame_2 = QFrame(self.frame)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setGeometry(QRect(30, 60, 161, 81))
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.frame_2)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(self.frame_2)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.lineEdit_2 = QLineEdit(self.frame_2)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+
+        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+
+        self.label_3 = QLabel(self.frame_2)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.lineEdit_3 = QLineEdit(self.frame_2)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.gridLayout.addWidget(self.lineEdit_3, 2, 1, 1, 1)
+
+        self.frame_3 = QFrame(self.frame)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setGeometry(QRect(20, 160, 191, 81))
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.gridLayout_2 = QGridLayout(self.frame_3)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.lineEdit_9 = QLineEdit(self.frame_3)
+        self.lineEdit_9.setObjectName(u"lineEdit_9")
+
+        self.gridLayout_2.addWidget(self.lineEdit_9, 2, 2, 1, 1)
+
+        self.label_5 = QLabel(self.frame_3)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout_2.addWidget(self.label_5, 1, 0, 1, 1)
+
+        self.lineEdit_6 = QLineEdit(self.frame_3)
+        self.lineEdit_6.setObjectName(u"lineEdit_6")
+
+        self.gridLayout_2.addWidget(self.lineEdit_6, 1, 1, 1, 1)
+
+        self.label_6 = QLabel(self.frame_3)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout_2.addWidget(self.label_6, 2, 0, 1, 1)
+
+        self.label_4 = QLabel(self.frame_3)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout_2.addWidget(self.label_4, 0, 0, 1, 1)
+
+        self.lineEdit_4 = QLineEdit(self.frame_3)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.gridLayout_2.addWidget(self.lineEdit_4, 0, 1, 1, 1)
+
+        self.lineEdit_5 = QLineEdit(self.frame_3)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+
+        self.gridLayout_2.addWidget(self.lineEdit_5, 0, 2, 1, 1)
+
+        self.lineEdit_7 = QLineEdit(self.frame_3)
+        self.lineEdit_7.setObjectName(u"lineEdit_7")
+
+        self.gridLayout_2.addWidget(self.lineEdit_7, 1, 2, 1, 1)
+
+        self.lineEdit_8 = QLineEdit(self.frame_3)
+        self.lineEdit_8.setObjectName(u"lineEdit_8")
+
+        self.gridLayout_2.addWidget(self.lineEdit_8, 2, 1, 1, 1)
+
+        self.frame_4 = QFrame(self.frame)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setGeometry(QRect(30, 260, 181, 61))
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.gridLayout_3 = QGridLayout(self.frame_4)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.lineEdit_10 = QLineEdit(self.frame_4)
+        self.lineEdit_10.setObjectName(u"lineEdit_10")
+
+        self.gridLayout_3.addWidget(self.lineEdit_10, 0, 1, 1, 1)
+
+        self.lineEdit_12 = QLineEdit(self.frame_4)
+        self.lineEdit_12.setObjectName(u"lineEdit_12")
+
+        self.gridLayout_3.addWidget(self.lineEdit_12, 0, 2, 1, 1)
+
+        self.label_7 = QLabel(self.frame_4)
+        self.label_7.setObjectName(u"label_7")
+
+        self.gridLayout_3.addWidget(self.label_7, 0, 0, 1, 1)
+
+        self.lineEdit_11 = QLineEdit(self.frame_4)
+        self.lineEdit_11.setObjectName(u"lineEdit_11")
+
+        self.gridLayout_3.addWidget(self.lineEdit_11, 1, 1, 1, 1)
+
+        self.lineEdit_13 = QLineEdit(self.frame_4)
+        self.lineEdit_13.setObjectName(u"lineEdit_13")
+
+        self.gridLayout_3.addWidget(self.lineEdit_13, 1, 2, 1, 1)
+
+        self.label_8 = QLabel(self.frame_4)
+        self.label_8.setObjectName(u"label_8")
+
+        self.gridLayout_3.addWidget(self.label_8, 1, 0, 1, 1)
+
+        self.frame_6 = QFrame(self.frame)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setGeometry(QRect(660, 420, 101, 121))
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.gridLayout_4 = QGridLayout(self.frame_6)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.label_11 = QLabel(self.frame_6)
+        self.label_11.setObjectName(u"label_11")
+
+        self.gridLayout_4.addWidget(self.label_11, 0, 0, 1, 1)
+
+        self.lineEdit_16 = QLineEdit(self.frame_6)
+        self.lineEdit_16.setObjectName(u"lineEdit_16")
+
+        self.gridLayout_4.addWidget(self.lineEdit_16, 0, 2, 1, 1)
+
+        self.lineEdit_19 = QLineEdit(self.frame_6)
+        self.lineEdit_19.setObjectName(u"lineEdit_19")
+
+        self.gridLayout_4.addWidget(self.lineEdit_19, 2, 2, 1, 1)
+
+        self.label_9 = QLabel(self.frame_6)
+        self.label_9.setObjectName(u"label_9")
+
+        self.gridLayout_4.addWidget(self.label_9, 1, 0, 1, 1)
+
+        self.label_10 = QLabel(self.frame_6)
+        self.label_10.setObjectName(u"label_10")
+
+        self.gridLayout_4.addWidget(self.label_10, 2, 0, 1, 1)
+
+        self.lineEdit_15 = QLineEdit(self.frame_6)
+        self.lineEdit_15.setObjectName(u"lineEdit_15")
+
+        self.gridLayout_4.addWidget(self.lineEdit_15, 1, 2, 1, 1)
+
+        self.lineEdit_14 = QLineEdit(self.frame_6)
+        self.lineEdit_14.setObjectName(u"lineEdit_14")
+
+        self.gridLayout_4.addWidget(self.lineEdit_14, 3, 2, 1, 1)
+
+        self.label_12 = QLabel(self.frame_6)
+        self.label_12.setObjectName(u"label_12")
+
+        self.gridLayout_4.addWidget(self.label_12, 3, 0, 1, 1)
+
+
+        self.page_2_layout.addWidget(self.frame)
+
         self.pages.addWidget(self.page_2)
         self.page_3 = QWidget()
         self.page_3.setObjectName(u"page_3")
@@ -55,7 +245,7 @@ class Ui_MainPages(object):
 
         self.retranslateUi(MainPages)
 
-        self.pages.setCurrentIndex(3)
+        self.pages.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(MainPages)
@@ -63,5 +253,17 @@ class Ui_MainPages(object):
 
     def retranslateUi(self, MainPages):
         MainPages.setWindowTitle(QCoreApplication.translate("MainPages", u"Form", None))
+        self.label.setText(QCoreApplication.translate("MainPages", u"Theta_1", None))
+        self.label_2.setText(QCoreApplication.translate("MainPages", u"Theta_2", None))
+        self.label_3.setText(QCoreApplication.translate("MainPages", u"Theta_3", None))
+        self.label_5.setText(QCoreApplication.translate("MainPages", u"P2", None))
+        self.label_6.setText(QCoreApplication.translate("MainPages", u"P3", None))
+        self.label_4.setText(QCoreApplication.translate("MainPages", u"P1", None))
+        self.label_7.setText(QCoreApplication.translate("MainPages", u"O2", None))
+        self.label_8.setText(QCoreApplication.translate("MainPages", u"O4", None))
+        self.label_11.setText(QCoreApplication.translate("MainPages", u"L1", None))
+        self.label_9.setText(QCoreApplication.translate("MainPages", u"L2", None))
+        self.label_10.setText(QCoreApplication.translate("MainPages", u"L3", None))
+        self.label_12.setText(QCoreApplication.translate("MainPages", u"L4", None))
     # retranslateUi
 
